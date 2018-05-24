c1471729ac7a6c00b1f4a21fdbee635d91ae6c6e
Andy Korth <andy@howlingmoonsoftware.com>
2018-05-23 19:08:06 -0500

    content and cleanup

--- Renderer/MetalRenderer.h
+++
@@ -1,9 +0,0 @@
-
-@import MetalKit;
-
-// Our platform independent render class
-@interface MetalRenderer : NSObject<MTKViewDelegate>
-
-- (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)mtkView;
-
-@end
---
+++ Renderer/MetalRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  MetalRenderer.swift
+//  MetalEngine-macOS
+//
+//  Created by Andy Korth on 5/23/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
